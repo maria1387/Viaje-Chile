4102d827ccb55411686b66d1c92e394cccc2d09a
maria1387 <mariapilar.abarca@gmail.com>
2021-08-09 18:04:06 -0400

    aplicando hover js y tooltip

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,17 +11,22 @@ $(function(){
 window.location.hash = gato;
 });
 } 
-});
-$( ".fas" ).click(function() {
-	$( ".pruebaimg" ).toggle( "slow", function() {
-	  // Animation complete.
-	});
+})
+
+  $( "h2.movi" ).hover(function() {
+	$( this ).fadeOut( 100 );
+	$( this ).fadeIn( 500 );
   });
 
-
+  
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-	return new bootstrap.Tooltip(tooltipTriggerEl)
+	return new bootstrap.Tooltip(tooltipTriggerEl)({
+	boundary: "window",
+	template:
+	'<div class="tooltip tooltip-custom" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'	
+	});
+
   })
   
   var myModal = document.getElementById('myModal')
@@ -30,5 +35,11 @@ var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
+
+
+
+
+
+
 });
 
